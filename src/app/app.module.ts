0211b266b46213ddf10e24dd5b934d39c9377801
unknown <ouldeabdellatif40@gmail.com>
2024-01-26 18:58:12 +0100

    update app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { SkillsComponent } from './skills/skills.component';
+import { MyWorksComponent } from './my-works/my-works.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    SkillsComponent,
+    MyWorksComponent
   ],
   imports: [
     BrowserModule
