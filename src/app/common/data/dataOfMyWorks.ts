338497f3f51c28fc19114580be118978dc523811
unknown <ouldeabdellatif40@gmail.com>
2024-02-29 22:57:07 +0100

    navbar: add interface and data

---
+++ src/app/common/data/dataOfMyWorks.ts
@@ -0,0 +1,46 @@
+import { Work } from "../interfaces/myWorks";
+
+export const works: Work[] = [
+    {
+        id: 1,
+        title: "Clip Santé",
+        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image: "../../assets/image/c-media.png",
+        link: "https://clipsante.fr/" 
+    },
+    {
+        id: 2,
+        title: "MesDocteurs",
+        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image: "../../assets/image/MesDocs.png",
+        link: "https://www.mesdocteurs.com/" 
+    },
+    {
+        id: 3,
+        title: "Juneyo",
+        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image: "../../assets/image/images.png",
+        link: "https://juneyo.com/" 
+    },
+    {
+        id: 4,
+        title: "H-Shop",
+        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image: "../../assets/image/images.png",
+        link: "https://juneyo.com/" 
+    },
+    {
+        id: 5,
+        title: "Flex",
+        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image: "../../assets/image/images.png",
+        link: "https://juneyo.com/" 
+    },
+    {
+        id: 5,
+        title: "Flex",
+        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image: "../../assets/image/images.png",
+        link: "https://juneyo.com/" 
+    }
+]
