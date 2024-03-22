2ac94a6006907d5a685952f9b5c75536e024bc43
unknown <ouldeabdellatif40@gmail.com>
2024-03-22 13:51:43 +0000

    remove for change the style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import { SkillsComponent } from './skills/skills.component';
-import { MyWorksComponent } from './my-works/my-works.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    ContactComponent,
-    SkillsComponent,
-    MyWorksComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
